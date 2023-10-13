2dad1aa6c0ada8b8d9c84a30a440d9861667d963
Aswin <aswinjikz071@gmail.com>
2023-10-13 10:08:32 +0530

    new updations

--- src/components/User/Home.jsx
+++ src/components/User/Home.jsx
@@ -58,7 +58,9 @@ function Home() {
             <h2 className="text-3xl font-semibold text-white-800 md:text-4xl">Search For Your <span className="text-indigo-600">Home</span></h2>
             <p className="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
             <div className="flex justify-center lg:justify-start mt-6">
+              <Link to='/properties'>
               <a className="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
+              </Link>
               <a className="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
             </div>
           </div>
